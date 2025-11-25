903bcee23bcf35679f5a0d086b86927758957635
laanhkiet012 <laanhkiet012@gmail.com>
2025-11-25 23:47:17 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll tn(ll n){
+    ll s=0;
+    while(n>0){
+        s+=n%10;
+        n/=10;
+    }
+    return s;
+}
+bool ct(ll n){
+    ll t=tn(n);
+    if(t%10==9){
+        return true;
+    }
+    return false;
+}
+int main()
+{
+    ll n;
+    cin>>n;
+    if(ct(n)){
+        cout<<"1";
+    }
+    else
+        cout<<"0";
+    return 0;
+}
