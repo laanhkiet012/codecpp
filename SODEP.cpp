3da4129a876e9a3fdb196025140ee5758e33cdcd
laanhkiet012 <laanhkiet012@gmail.com>
2025-11-26 00:10:40 +0700

    Update SODEP.cpp

--- SODEP.cpp
+++ SODEP.cpp
@@ -6,17 +6,17 @@ int tong(string &n){
 	int t=0;
 	for(int i=0;i<n.length();i++){
 		char c = n[i];
-		t=(t+(c-48));
+		t+=(c-48);
 	}
 	return t;
 }
 
-main(){
-	freopen("SODEP.INP", "r", stdin); 
+int main(){
+	freopen("SODEP.INP", "r", stdin);
     freopen("SODEP.OUT", "w", stdout);
     string n;
     cin>>n;
 	if(tong(n)%10 == 9)
-	cout<<1;
+        cout<<1;
 	else cout<<0;
 }
